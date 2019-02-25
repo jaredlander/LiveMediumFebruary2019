b6e7650e1c18ad95353f874f9eb9ec349fa3dc4e
Jared Lander <info@jaredlander.com>
2019-02-25 11:59:52 -0500

    Read tables

--- code/ReadHTML.r
+++ code/ReadHTML.r
@@ -20,3 +20,21 @@ ribalta %>% html_nodes('.street') %>% html_text()
 ribalta %>% html_nodes('#latitude')
 ribalta %>% html_nodes('span#latitude')
 ribalta %>% html_nodes('#latitude') %>% html_attr('value')
+
+
+ribalta %>% 
+    html_nodes('table.food-items')
+
+ribalta %>% 
+    html_nodes('table.food-items') %>% 
+    magrittr::extract2(5) %>% 
+    html_table()
+
+ribalta %>% 
+    html_nodes('table.food-items') %>% 
+    purrr::map(html_table, fill=TRUE)
+
+ribalta %>% 
+    html_nodes('table.food-items') %>% 
+    purrr::map_df(html_table, fill=TRUE, .id='Type') %>% 
+    tibble::as_tibble()

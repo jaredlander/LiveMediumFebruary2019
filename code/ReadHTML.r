f9734730c8c4b5d209f37743978b22167071e0c2
Jared Lander <info@jaredlander.com>
2019-02-25 11:50:10 -0500

    Showed parsing nodes

---
+++ code/ReadHTML.r
@@ -0,0 +1,18 @@
+library(rvest)
+
+ribalta <- read_html('data/ribalta.html')
+class(ribalta)
+
+ribalta %>% html_nodes('ul')
+ribalta %>% html_nodes('ul') %>% html_nodes('li')
+ribalta %>% html_nodes('ul') %>% html_nodes('li.address')
+ribalta %>% html_nodes('ul') %>% html_nodes('li.address') %>% html_nodes('span.street')
+
+ribalta %>% 
+    html_nodes('ul') %>% 
+    html_nodes('li.address') %>% 
+    html_nodes('span.street') %>% 
+    html_text()
+
+ribalta %>% html_nodes('.street')
+ribalta %>% html_nodes('.street') %>% html_text()

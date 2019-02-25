49ab9f54cd06ec851fb37ce52dc98481bbd0c994
Jared Lander <info@jaredlander.com>
2019-02-25 13:10:27 -0500

    Showed spread

--- code/tidyr.r
+++ code/tidyr.r
@@ -6,3 +6,8 @@ library(tidyr)
 reaction %>% gather(key=Stat, value=Data, Age, BMI, React, Regulate)
 reaction %>% gather(key=Stat, value=Data, Age, BMI, React, Regulate) %>% 
     dplyr::arrange(ID, Test)
+
+reactLong <- reaction %>% gather(key=Stat, value=Data, Age, BMI, React, Regulate)
+reactLong
+
+reactLong %>% spread(key=Stat, value=Data)
